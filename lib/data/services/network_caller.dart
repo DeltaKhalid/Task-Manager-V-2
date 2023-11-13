850bab19b9e04322c5a629a2b5c4284aecdd367b
DeltaKhalid <khalid.delta063@gmail.com>
2023-11-13 10:02:20 +0600

    Network Caller has been added

---
+++ lib/data/services/network_caller.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:http/http.dart';
+import 'package:task_managet/data/models/network_response.dart';
+
+
+class NetworkCaller {
+  Future<NetworkResponse> getRequest(String url) async{
+    //Map<String, dynamic>? decodedResponse;
+    try{
+      Response response = await get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        //decodedResponse = jsonDecode(response.body);
+        //final decodedResponse = jsonDecode(response.body);
+        return NetworkResponse(true, response.statusCode, jsonDecode(response.body));
+      } else {
+        //todo - handle other response code - 401,400, 500
+        return NetworkResponse(false, response.statusCode, null);
+      }
+    } catch (e) {
+      log(e.toString());      ///*** Replace of print method
+    }
+    return NetworkResponse(false, -1, null);
+
+    //return decodedResponse;
+
+    // finally {
+    //   return decodedResponse;
+    // }
+  }
+  Future<NetworkResponse> postRequest(String url, Map<String, dynamic> body) async{
+    //Map<String, dynamic>? decodedResponse;
+    try{
+      Response response = await post(Uri.parse(url), body: jsonEncode(body));
+      if (response.statusCode == 200) {
+        //decodedResponse = jsonDecode(response.body);
+        //final decodedResponse = jsonDecode(response.body);
+        return NetworkResponse(true, response.statusCode, jsonDecode(response.body));
+      } else {
+        //todo - handle other response code - 401,400, 500
+        return NetworkResponse(false, response.statusCode, null);
+      }
+    } catch (e) {
+      log(e.toString());      ///*** Replace of print method
+    }
+    return NetworkResponse(false, -1, null);
+
+    //return decodedResponse;
+
+    // finally {
+    //   return decodedResponse;
+    // }
+  }
+
+
+}
